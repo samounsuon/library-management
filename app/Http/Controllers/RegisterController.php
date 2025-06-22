7b6bfb94956c30031d88c21d003357fddc5381b2
Suon Samoun <samoun.suon@student.passerellesnumeriques.org>
2025-06-22 12:33:16 +0700

    push laravel project

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Register;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
-    //
+    function  index()
+    {
+        $user= DB::table('registers')->get();
+        return response()->json($user);
+    }
+    function store(Request $request)
+    {
+        $register= new Register();
+        $register->name = $request->name;
+        $register->email = $request->email;
+        $register->phone = $request->phone;
+        $register->gender= $request->gender;
+        $register->password = bcrypt($request->password);
+        $register->save();
+        return response()->json([
+            'message' => 'User registered successfully',
+            'user' => $register
+        ], 201);
+    }
 }
